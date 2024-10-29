29e22951df31f692efba3108eecc6a54b2ab495b
Asrul Hidayat <136725199+AsrulHidayat@users.noreply.github.com>
2024-10-29 09:44:44 +0800

    Comments

--- bin/belajar_dart.dart
+++ bin/belajar_dart.dart
@@ -1,5 +1,22 @@
 import 'package:belajar_dart/belajar_dart.dart' as belajar_dart;
 
+// void main(List<String> arguments) {
+//   print('Hello world: ${belajar_dart.calculate()}!');
+// }
+
+/// Fungsi [main] akan menampilkan 2 output
+/// Output pertama menampilkan teks dan output kedua menampilkan hasil perkalian pada fungsi [calculate]
 void main(List<String> arguments) {
-  print('Hello world: ${belajar_dart.calculate()}!');
+  // Mencetak Hello Dart! Dart is great. pada konsol
+  print('Hello Dart! Dart is great.');
+  // Testing documentation comment with [].
+  print('6 * 7 = ${calculate()}');
 }
+
+int calculate() {
+  return 6 * 7;
+}
+
+/// Output:
+/// Hello Dart! Dart is great.
+/// 6 * 7 = 42
