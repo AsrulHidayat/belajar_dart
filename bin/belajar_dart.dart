4490e684311a98311d8367dbcf86b1f75314af0b
Asrul Hidayat <136725199+AsrulHidayat@users.noreply.github.com>
2024-10-29 09:54:58 +0800

    Variables

--- bin/belajar_dart.dart
+++ bin/belajar_dart.dart
@@ -1,22 +1,34 @@
 import 'package:belajar_dart/belajar_dart.dart' as belajar_dart;
 
+// 1
 // void main(List<String> arguments) {
 //   print('Hello world: ${belajar_dart.calculate()}!');
 // }
 
-/// Fungsi [main] akan menampilkan 2 output
-/// Output pertama menampilkan teks dan output kedua menampilkan hasil perkalian pada fungsi [calculate]
-void main(List<String> arguments) {
-  // Mencetak Hello Dart! Dart is great. pada konsol
-  print('Hello Dart! Dart is great.');
-  // Testing documentation comment with [].
-  print('6 * 7 = ${calculate()}');
-}
 
-int calculate() {
-  return 6 * 7;
-}
+// 2
+// /// Fungsi [main] akan menampilkan 2 output
+// /// Output pertama menampilkan teks dan output kedua menampilkan hasil perkalian pada fungsi [calculate]
+// void main(List<String> arguments) {
+//   // Mencetak Hello Dart! Dart is great. pada konsol
+//   print('Hello Dart! Dart is great.');
+//   // Testing documentation comment with [].
+//   print('6 * 7 = ${calculate()}');
+// }
+//
+// int calculate() {
+//   return 6 * 7;
+// }
+//
+// /// Output:
+// /// Hello Dart! Dart is great.
+// /// 6 * 7 = 42
 
-/// Output:
-/// Hello Dart! Dart is great.
-/// 6 * 7 = 42
+
+// 3
+void main() {
+  var myAge; myAge = 20;
+  var greetings = 'Hello Dart!';
+  print(greetings);
+  print(myAge);
+}
