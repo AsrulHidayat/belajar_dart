2536750af6ddfa1601e2ea8c836fc22caf3a22c7
Asrul Hidayat <136725199+AsrulHidayat@users.noreply.github.com>
2024-10-30 15:12:13 +0800

    If and Else

--- bin/belajar_dart.dart
+++ bin/belajar_dart.dart
@@ -707,13 +707,73 @@ import 'package:belajar_dart/belajar_dart.dart' as belajar_dart;
 // }
 
 // cara 2
+// void main() {
+//   String? favoriteFood = 'Mie Ayam';
+//
+//   buyAMeal(favoriteFood!);      // disematkan bang operator supaya mengembalikan nilai non-null
+// }
+//
+// void buyAMeal(String favoriteFood) {
+//   print('I bought a $favoriteFood');
+// }
+
+
+// CONTROL FLOW
+
+// 1. If and Else
+
+// void main() {
+//   var isRaining = true;
+//
+//   print('Prepare before going to office.');
+//   if (isRaining) {
+//     print("Oh. It's raining, bring an umbrella.");
+//   }
+//   print('Going to the office.');
+// }
+
+// Jika Anda mengubah nilai isRaining menjadi false, maka kode di dalam blok if akan dilewatkan dan program Anda tidak akan mengingatkan untuk membawa payung.
+
+// void main() {
+//   var openHours = 8;
+//   var closedHours = 21;
+//   var now = 17;
+//
+//   if (now > openHours && now < closedHours) {
+//     print("Hello, we're open");
+//   } else {
+//     print("Sorry, we've closed");
+//   }
+// }
+
+// contoh bahwasanya output else if disesuakan dengan urutan code
+
 void main() {
-  String? favoriteFood = 'Mie Ayam';
+  var score = 85;
 
-  buyAMeal(favoriteFood!);      // disematkan bang operator supaya mengembalikan nilai non-null
+  print('Nilai Anda: ${calculateScore(score)}');
 }
 
-void buyAMeal(String favoriteFood) {
-  print('I bought a $favoriteFood');
+String calculateScore(num score) {
+  if (score > 90) {
+    return 'A';
+  } else if (score > 80) {
+    return 'B';
+  } else if (score > 70) {
+    return 'C';
+  } else if (score > 60) {
+    return 'D';
+  } else {
+    return 'E';
+  }
 }
+
+// Fitur menarik lain dari Dart adalah conditional expressions. Dengan ini kita bisa menuliskan if-else statement hanya dalam satu baris:
+
+// condition ? true expression : false expression
+
+// var shopStatus = now > openHours ? "Hello, we're open" : "Sorry, we've closed";
+
+// // expression1 ?? expression2
+// var buyer = name ?? 'user';
 
