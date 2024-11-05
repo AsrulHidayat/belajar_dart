fc9d5cb3e96b25e773d43aff33837a97a641a40e
Asrul Hidayat <136725199+AsrulHidayat@users.noreply.github.com>
2024-11-05 09:55:17 +0800

    Panduan Effective Dart

--- bin/effective_dart.dart
+++ bin/effective_dart.dart
@@ -1,6 +1,6 @@
-// EFFECTIVE DART
+/// EFFECTIVE DART
 
-// 1. Pengenalan Effective Dart
+/// 1. Pengenalan Effective Dart
 
 // Coding convention adalah panduan untuk bahasa pemrograman tertentu yang merekomendasikan gaya pemrograman, praktik, dan metode untuk setiap aspek program yang ditulis dalam bahasa tersebut. Konvensi ini biasanya meliputi indentasi, komentar, deklarasi, penamaan, arsitektur, dll.
 
@@ -16,6 +16,22 @@
 // Be consistent (Konsisten). Ketika membahas tentang formatting akan banyak argumen subjektif tentang mana yang lebih baik. Namun konsistensi tentunya akan sangat membantu secara objektif.
 
 // Be brief (Ringkas). Dart dirancang supaya familiar dengan bahasa pemrograman lain seperti C, Java, JavaScript, dll. Namun Dart ditambah dengan fitur-fitur lain yang belum ditawarkan bahasa lain. Jika ada banyak cara untuk mengungkapkan sesuatu, tentu Anda akan menggunakan cara yang paling ringkas bukan?
+
+
+/// 2. Panduan Effective Dart
+
+// - Style Guide, aturan dan tata letak kode
+// - Docuumentation guide, apa yang boleh dan tidak ada di komentar
+// - Usage guide, pemaanfaatan fitur bahasa secara terbaik untuk menerapkan perilaku.
+// - Design Guide, mencakup secara luas dan tidak mengikat, seperti bagaimana mendesain API libarary yang konsisten dan bisa digunakan.
+
+/// Rules
+
+// DO, praktik tersebut harus dilakukan
+// DONT, bukan merupakan hal yang baik untuk diterapkan
+// PREFER, praktik yang harus diikuti tapi ada keadaan juga yang mungkin lebih masuk akal untuk dilakukan.
+// AVOID, kebalikan dari PREFER, menjelaskan hal-hal yang tidak boleh dilakukan, namun kemungkinan ada alasan bagus untuk melakukannya pada beberapa kejadian.
+// CONSIDER, praktik yang bisa diikuti atau tidak anda ikuti, tergantung pada keadaan dan preferensi sendiri.
 
 
 
